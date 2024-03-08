3e8767834a8f946e60b1f906c71570cc83884b66
Jorge040599 <158073624+Jorge040599@users.noreply.github.com>
2024-03-08 16:26:12 -0600

    Segunda actualización codigo - Jorge
    Se configura la ventana y se cargan las imágenes llamándolas.

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ class Gato:
         # Colores
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
+
+        # Configuración de la ventana
+        self.window = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption("Gato")
+
+        # Cargar imágenes
+        self.X_IMAGE = pygame.image.load(r'C:\Users\urani\Desktop\Programa Gato\X.png')
+        self.O_IMAGE = pygame.image.load(r'C:\Users\urani\Desktop\Programa Gato\O.png')
